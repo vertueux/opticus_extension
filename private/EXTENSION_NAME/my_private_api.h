ffcfe5ce58ca080ee182e65a092f1682ce271e21
Noah VERTUEUX <81981323+vertueux@users.noreply.github.com>
2022-08-15 14:05:38 +0200

    Updated the exporting DLL method.
    Using CMake tools.

--- private/EXTENSION_NAME/my_private_api.h
+++ private/EXTENSION_NAME/my_private_api.h
@@ -2,10 +2,9 @@
 
 // Rename this file & put here all sources being intended to be used in the extension in this directory.
 
-#include "extension/api/extension_dll_export.h"
 namespace extension_name {
 
-class EXTENSION_API PrivateExtensionName {
+class PrivateExtensionName {
  public: 
   void your_private_void();
 };
--- public/EXTENSION_NAME/my_public_api.h
+++ public/EXTENSION_NAME/my_public_api.h
@@ -1,9 +1,8 @@
 // Rename this file & put here all sources being intended to be used in the client side in this directory.
 
-#include "extension/api/extension_dll_export.h"
 namespace extension_name {
 
-class EXTENSION_API PublicExtensionName {
+class PublicExtensionName {
  public: 
   void your_public_void();
 };
